03eacab72839b49ce55c3fd835639d8930a003cb
xujw3 <kingxujw3@gmail.com>
2025-02-15 14:28:50 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,6 @@ sed -i 's/OpenWrt/OpenWrtKing/g' openwrt/package/base-files/files/bin/config_gen
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+#去除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
